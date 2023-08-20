3cceb31d066476c3f26712820256fe61fd24c0d2
qa test <aleksandrbabkin238@gmail.com>
2023-08-20 20:19:09 +0500

    D3Z1

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+
+    public int calculate(int a){
+        int result;
+        int consOfMiles = 20;
+        result = a / consOfMiles;
+        return result;
+    }
+}
